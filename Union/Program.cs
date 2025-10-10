8afc63045e3f54a45752818ba77359afae78b0a3
Medhat3760 <Medhat3760@gmail.com>
2025-10-11 01:37:11 +0200

    Added a union data structure

---
+++ Union/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Union
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct MyUnion
+    {
+        [FieldOffset(0)]
+        public int intValue;
+
+        [FieldOffset(0)]
+        public float floatValue;
+
+        [FieldOffset(0)]
+        public char charValue;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            MyUnion myUnion;
+
+            myUnion.intValue = 42;
+            Console.WriteLine("Integer value: " + myUnion.intValue);
+
+            myUnion.floatValue = 3.14f;
+            Console.WriteLine("Float value: " + myUnion.floatValue);
+
+            myUnion.charValue = 'A';
+            Console.WriteLine("Char value: " + myUnion.charValue);
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
